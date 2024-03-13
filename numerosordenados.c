23ba3a1bfa5f134919e29ab075a367cb982bdf23
MelMontenegro <162513495+MelMontenegro@users.noreply.github.com>
2024-03-12 22:10:19 -0300

    resolvendo problema no codigo

--- numerosordenados.c
+++ numerosordenados.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
  //!showArray(v, cursors=[j])
 int main () {
-int n, j, x;
+int n, j, x, i;
 scanf("%d, &n");
 int v[n + 1];
 for (j = 0; j < n; j ++) {
@@ -9,7 +9,7 @@ for (j = 0; j < n; j ++) {
 }
 printf("Informe um numero\n");
 scanf("%d", &x);
-for (j = n - 1; j >= 0 && x < v[j]; j--) {
+for (j = i - 1; j >= 0 && x < v[j]; j--) {
     v[j + 1] = v[j];
  }
  v[j + 1] = x;
