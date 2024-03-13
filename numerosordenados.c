dac5487a39b1a7b268a14913a794e254aa4cb74d
MelMontenegro <162513495+MelMontenegro@users.noreply.github.com>
2024-03-12 21:51:57 -0300

    calculadora simples.

---
+++ numerosordenados.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ //!showArray(v, cursors=[j])
+int main () {
+int n, j, x;
+scanf("%d, &n");
+int v[n + 1];
+for (j = 0; j < n; j ++) {
+    scanf ("%d", &v[j]);
+}
+printf("Informe um numero\n");
+scanf("%d", &x);
+for (j = n - 1; j >= 0 && x < v[j]; j--) {
+    v[j + 1] = v[j];
+ }
+ v[j + 1] = x;
+ for (j = 0; j < n + 1; j++); {
+    printf("%d", v[j]);
+ }
+}
